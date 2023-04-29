3c07892caba60201104b2b170f111ac4962a0bb7
keisuke1206 <keisukeeee12061206@gmail.com>
2023-04-29 18:27:56 +0900

    topページの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
-  get 'sample/top', to: 'sample#top'
+  root :to => 'sample/top', to: 'sample#top'
   get 'sample/login', to: 'sample#login'
+  get 'sample/new_login', to: 'sample#new_login'
 end
