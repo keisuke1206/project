188c6decd22d8584c71b462b845f2a302a1e1528
keisuke1206 <keisukeeee12061206@gmail.com>
2023-05-08 08:04:05 +0900

    url変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root :to => 'sample/top', to: 'sample#top'
-  get 'sample/login', to: 'sample#login'
-  get 'sample/new_login', to: 'sample#new_login'
+  get 'login', to: 'sample#login'
+  get 'new_login', to: 'sample#new_login'
 end
